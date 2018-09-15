14dfc702f2aaee835a81543d23ed4a205b93f132
小徐 <yixu_cs@hust.edu.cn>
2018-09-15 11:03:58 +0800

    add

--- code/拼出2018.cpp
+++ code/拼出2018.cpp
@@ -6,79 +6,52 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-const int MAX_N = 2018, MAX_V = 2018; 
-int dp[MAX_N][MAX_V];
-bool flag[MAX_N][MAX_V];
-
-bool compare(int a,int b)
+void core(const vector<int>& A, int index,int k, int sum, int target,vector<int>& cur,vector<vector<int> >& res)
 {
- return a>b;
+    if(sum == target)
+    {
+        res.push_back(cur);
+        return;
+    }
+
+    if(index == A.size())
+        return;
+    if(sum > target)
+        return;
+
+    cur.push_back(A[index]);
+    core(A,index+1,k,sum+A[index],target,cur,res);
+    cur.pop_back();
+    core(A,index+1,k,sum,target,cur,res);
 }
 
-std::vector<int> MakeUp2018(std::vector<int> num)
+vector<int> KSum(vector<int>& A,int k,int target)
 {
-
-	sort(num.begin(),num.end(),compare);
-	int n = num.size();
-
-	memset(dp,-1,sizeof(dp));
-	for(int i=0;i<=num.size();i++)
+    vector<int> cur;
+    vector<vector<int> > res;
+    core(A,0,k,0,target,cur,res);
+    int min = 0x7FFFFFFF;
+    int minIndex = -1;
+    for(int i = 0;  i < res.size(); ++i)
     {
-        dp[i][0]=0;
-    }
-	memset(flag,false,(n+1)*(MAX_V+1)*sizeof(bool));
-
-	bool isFind = false;
-
-	for(int i=1; i<=n ;i++)
-    {
-        for(int j=1;j<=MAX_V;j++)
+        if(res[i].size() < min)
         {
-            if(dp[i-1][j-num[i-1]]!=-1)
-            {
-                dp[i][j]=dp[i-1][j];
-                if(num[i-1]<=j)
-                {
-                	
-                    dp[i][j]=dp[i-1][j-num[i-1]]+num[i-1];
-                    flag[i][j]=true;
-                    //由于一定有一个解，而且装背包是从物品的质量从大到小装的，
-                    //所以得到第一个满足的解就可以跳出
-                    if(dp[i][j] == 2018)
-                    {
-                    	isFind = true;
-                    	break;
-                    }
-                }
-            }
-        } 
-        if(isFind == true)
-            break;
+            min = res[i].size();
+            minIndex = i;
+        }
     }
-
-    int i = num.size();
-    int j = MAX_V;
-    std::vector<int> result;
-    while(i >= 0 && j >= 0)
-    {
-    	if(flag[i][j])
-    	{
-    		result.push_back(num[i-1]);
-    		j = j - num[i-1];
-    	}
-    	--i;
-    }
-    return result;
+    return res[minIndex];
 }
 
+//这里我们不用限制k的取值范围
 int main()
 {
 	vector<int> n;
-	n.push_back(1000);
-	n.push_back(1000);
-	n.push_back(9);
-	n.push_back(9);
-	vector<int> m = MakeUp2018(n);
+	n.push_back(2016);
+	n.push_back(2018);
+	n.push_back(1);
+	n.push_back(1);
+	vector<int> m = KSum(n,0,2018);
 	for(int i = 0 ; i < m.size(); ++i)
 		cout<<m[i]<<endl;
 }
